9cfec982bc88da1ba20d3cf9e3114da6ffd6be74
George Czabania <1140167+stayradiated@users.noreply.github.com>
2020-02-12 16:09:03 +1300

    feat: stop cleaning dist folder for faster builds

--- src/cmd/build.ts
+++ src/cmd/build.ts
@@ -2,13 +2,9 @@ import { SRC_PATH, DIST_PATH } from '../shared/constants'
 import { log, fmt } from '../shared/log'
 import exec from '../shared/exec'
 
-import clean from '../cmd/clean'
-
 const TSC_SHIM_PATH = require.resolve('../shim/tsc')
 
 const build = async () => {
-  await clean()
-
   log(fmt`Building from ${SRC_PATH} to ${DIST_PATH} directory`)
 
   const args = process.argv.slice(2)
