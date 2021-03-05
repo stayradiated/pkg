9c52b789bb8b7bb7f2403fee73e077ab10a8b6ee
George Czabania <1140167+stayradiated@users.noreply.github.com>
2021-03-06 11:23:53 +1300

    chore: fix up ava-* shims

--- src/shim/ava-plain.ts
+++ src/shim/ava-plain.ts
@@ -9,20 +9,28 @@ const start = async () => {
   const config = await getConfig()
   const filepath = readPkgUp.sync().path
 
-  mockWithContext('pkg-conf', require.resolve('ava/cli'), () => ({
-    sync: (pkgName: string, opts: Record<string, unknown>) => {
-      if (pkgName === 'ava') {
-        return config
-      }
-      return pkgConf.sync(pkgName, opts)
-    },
-    filepath: (c: Config) => {
-      if (c === config) {
-        return filepath
-      }
-      return pkgConf.filepath(c)
-    },
-  }))
+  const mockedPkgConf = async (
+    pkgName: string,
+    opts: Record<string, unknown>,
+  ) => {
+    return mockedPkgConf.sync(pkgName, opts)
+  }
+
+  mockedPkgConf.sync = (pkgName: string, opts: Record<string, unknown>) => {
+    if (pkgName === 'ava') {
+      return config
+    }
+    return pkgConf.sync(pkgName, opts)
+  }
+
+  mockedPkgConf.filepath = (c: Config) => {
+    if (c === config) {
+      return filepath
+    }
+    return pkgConf.filepath(c)
+  }
+
+  mockWithContext('pkg-conf', require.resolve('ava/cli'), () => mockedPkgConf)
 
   return require('ava/cli')
 }
--- src/shim/ava-tsc.ts
+++ src/shim/ava-tsc.ts
@@ -1,24 +1,38 @@
 import { mockWithContext } from 'unwire'
 import * as pkgConf from 'pkg-conf'
-
-import { SRC_PATH } from '../shared/constants'
+import { Config } from 'pkg-conf'
+import readPkgUp from 'read-pkg-up'
 
 import getAVAConfig from '../config/ava-tsc'
 
 console.log('Using AVA with ts-node')
 
 const start = async () => {
-  const avaConfig = await getAVAConfig()
+  const config = await getAVAConfig()
+  const filepath = readPkgUp.sync().path
 
-  mockWithContext('pkg-conf', require.resolve('ava/cli'), () => ({
-    sync: (pkgName: string, opts: Record<string, unknown>) => {
-      if (pkgName === 'ava') {
-        return avaConfig
-      }
-      return pkgConf.sync(pkgName, opts)
-    },
-    filepath: () => SRC_PATH,
-  }))
+  const mockedPkgConf = async (
+    pkgName: string,
+    opts: Record<string, unknown>,
+  ) => {
+    return mockedPkgConf.sync(pkgName, opts)
+  }
+
+  mockedPkgConf.sync = (pkgName: string, opts: Record<string, unknown>) => {
+    if (pkgName === 'ava') {
+      return config
+    }
+    return pkgConf.sync(pkgName, opts)
+  }
+
+  mockedPkgConf.filepath = (c: Config) => {
+    if (c === config) {
+      return filepath
+    }
+    return pkgConf.filepath(c)
+  }
+
+  mockWithContext('pkg-conf', require.resolve('ava/cli'), () => mockedPkgConf)
 
   return require('ava/cli')
 }
