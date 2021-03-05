783e2b72122f0a4066a13b2af2ee15e3ecedf1ce
George Czabania <1140167+stayradiated@users.noreply.github.com>
2021-03-06 10:42:21 +1300

    chore: upgrade dependencies

--- src/cmd/docs.ts
+++ src/cmd/docs.ts
@@ -3,11 +3,11 @@ import { log, fmt } from '../shared/log'
 
 import typedoc from '../shim/typedoc'
 
-const build = async (): Promise<void> => {
+const docs = async (): Promise<void> => {
   log(fmt`Building docs from ${SRC_PATH} to ${DOCS_PATH} directory`)
 
   log(fmt`Running ${'typedoc'}`)
   await typedoc(SRC_PATH, DOCS_PATH, DOCS_EXTERNAL)
 }
 
-export default build
+export default docs
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default as build } from './cmd/build'
+export { default as clean } from './cmd/clean'
+export { default as docs } from './cmd/docs'
+export { default as lint } from './cmd/lint'
+export { default as precommit } from './cmd/precommit'
+export { default as test } from './cmd/test'
+export { default as tidy } from './cmd/tidy'
